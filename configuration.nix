844475832ad6b09830f684a170bb417d88664a09
seevuh <119629904+seevuh@users.noreply.github.com>
2026-05-21 23:19:25 +0530

    refactor

--- configuration.nix
+++ configuration.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, user, ... }:
 
 {
@@ -21,8 +17,13 @@
   # Use latest kernel.
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
   networking = {
     hostName = "nixos"; # Define your hostname
+
+    networkmanager.enable = false; # Enabling WPA instead
 
     # static ip
     interfaces.wlo1 = {
@@ -48,13 +49,6 @@
       };
     };
   };
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = false;
 
   # Set your time zone.
   time.timeZone = "Asia/Kolkata";
@@ -98,37 +92,13 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
   services.xserver.libinput.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.${user} = {
-    isNormalUser = true;
-    description = "sea vuh";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-    #  thunderbird
-    ];
-  };
-
-  # User does not need to give password when using sudo
-  security = {
-    sudo.wheelNeedsPassword = false;
-  };
-
-  # Install firefox.
-  programs.firefox.enable = true;
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
 
   # Enable Flatpak service
   services.flatpak.enable = true;
@@ -148,44 +118,7 @@
     extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
   };
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
-  git
-  # killall
-  # usbutils
-  # pciutils
-  # wget
-  # xterm
-  ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-
-  # Enable VSCode Server
-  programs.nix-ld.enable = true;
-
   # Disable sleep
-  # systemd.sleep.settings = {
-  #   Sleep = {
-  #     AllowSuspend = "yes";
-  #     AllowHibernation = "no";
-  #     AllowHybridSleep = "no";
-  #     AllowSuspendThenHibernate = "no";
-  #   };
-  # };
   systemd.sleep.extraConfig = ''
     AllowSuspend=no
     AllowHibernate=no
@@ -193,9 +126,39 @@
     AllowSuspendThenHibernate=no
   '';
 
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.${user} = {
+    isNormalUser = true;
+    description = "sea vuh";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+    #  thunderbird
+    ];
+  };
+
+  # User does not need to give password when using sudo
+  security = {
+    sudo.wheelNeedsPassword = false;
+  };
+
+  # Install firefox.
+  programs.firefox.enable = true;
+
+  # Enable VSCode Server
+  programs.nix-ld.enable = true;
+
+
+  environment.systemPackages = with pkgs; [
+    vim
+    git
+  ];
+
   # Enable Auto Garbage Collect
   nix = {
-    settings.auto-optimise-store = true;
+    settings = {
+      experimental-features = [ "nix-command" "flakes" ];
+      auto-optimise-store = true;
+    };
     gc = {
       automatic = true;
       dates = "weekly";
@@ -207,7 +170,6 @@
   system.autoUpgrade = {
     enable = true;
     allowReboot = true;
-    # channel = "https://channels.nixos.org/nixos-25.11";
     dates = "daily";
     rebootWindow = {
       lower = "01:00";
@@ -216,21 +178,5 @@
     runGarbageCollection = true;
   };
 
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.05"; # Did you read the comment?
-
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
+  system.stateVersion = "25.011";
 }
